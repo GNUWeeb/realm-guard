aa5437dce6386f34df5464d6ee88747e1f709ba5
Irvan Malik <irvanmalik48@gmail.com>
2022-12-26 04:36:49 +0700

    feat(defaults.ts): generate default data
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

---
+++ defaults.ts
@@ -0,0 +1,13 @@
+import * as fs from "fs";
+import { getStorageDir } from "./modules/generic";
+
+export function createDefaults() {
+  // create default greets.json
+  if (!fs.existsSync(getStorageDir() + "groups/")) {
+    fs.mkdirSync(getStorageDir() + "groups/");
+    fs.writeFileSync(getStorageDir() + "groups/default.json", JSON.stringify({
+      welcome: "Welcome to {{group_name}}, {{user_name}}!",
+      farewell: "Goodbye, {{user_name}}!"
+    }, null, 2));
+  }
+}
