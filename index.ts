f2a564efeab72b54f197f8408a83446d85f89251
Irvan Malik <irvanmalik48@gmail.com>
2022-12-26 00:11:57 +0700

    feat(index.ts): add unmute command
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { bot } from "./config";
 import { startCommand, versionCommand, pingCommand, helpCommand } from "./modules/generic";
 import { banCommand, unbanCommand } from "./modules/admin/ban";
 import { kickCommand } from "./modules/admin/kick";
+import { unmuteCommand } from "./modules/admin/mute";
 
 bot.command(startCommand.command, startCommand.function);
 bot.command(versionCommand.command, versionCommand.function);
@@ -12,6 +13,7 @@ bot.command(banCommand.command, banCommand.function);
 bot.command(unbanCommand.command, unbanCommand.function);
 
 bot.command(kickCommand.command, kickCommand.function);
+bot.command(unmuteCommand.command, unmuteCommand.function);
 
 bot.launch();
 
