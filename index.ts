b109d203b35de406870dd85f7435aae6f9940e2f
Irvan Malik <irvanmalik48@gmail.com>
2022-12-26 00:53:00 +0700

    feat(index.ts): insantiate modules using foreach
    DRY (don't repeat yourself)
    
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- index.ts
+++ index.ts
@@ -1,20 +1,9 @@
 import { bot } from "./config";
-import { startCommand, versionCommand, pingCommand, helpCommand } from "./modules/generic";
-import { banCommand, unbanCommand } from "./modules/admin/ban";
-import { kickCommand } from "./modules/admin/kick";
-import { muteCommand, unmuteCommand } from "./modules/admin/mute";
+import { commands } from "./modules/mod";
 
-bot.command(startCommand.command, startCommand.function);
-bot.command(versionCommand.command, versionCommand.function);
-bot.command(pingCommand.command, pingCommand.function);
-bot.command(helpCommand.command, helpCommand.function);
-
-bot.command(banCommand.command, banCommand.function);
-bot.command(unbanCommand.command, unbanCommand.function);
-
-bot.command(kickCommand.command, kickCommand.function);
-bot.command(muteCommand.command, muteCommand.function);
-bot.command(unmuteCommand.command, unmuteCommand.function);
+commands.forEach((command) => {
+  bot.command(command.command, command.function);
+});
 
 bot.launch();
 
