8fb0efecc15027c9e01180cc9d25da4d7c5150a7
Irvan Malik <irvanmalik48@gmail.com>
2022-12-26 00:12:51 +0700

    feat(index.ts): add mute command
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- index.ts
+++ index.ts
@@ -2,7 +2,7 @@ import { bot } from "./config";
 import { startCommand, versionCommand, pingCommand, helpCommand } from "./modules/generic";
 import { banCommand, unbanCommand } from "./modules/admin/ban";
 import { kickCommand } from "./modules/admin/kick";
-import { unmuteCommand } from "./modules/admin/mute";
+import { muteCommand, unmuteCommand } from "./modules/admin/mute";
 
 bot.command(startCommand.command, startCommand.function);
 bot.command(versionCommand.command, versionCommand.function);
@@ -13,6 +13,7 @@ bot.command(banCommand.command, banCommand.function);
 bot.command(unbanCommand.command, unbanCommand.function);
 
 bot.command(kickCommand.command, kickCommand.function);
+bot.command(muteCommand.command, muteCommand.function);
 bot.command(unmuteCommand.command, unmuteCommand.function);
 
 bot.launch();
