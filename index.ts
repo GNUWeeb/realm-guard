ae82347d1afeff1cf287da957dec4857cb55b753
Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>
2022-12-26 02:28:51 +0700

    feat(modules/admin/warn.ts): Add warn admin module
    This module contains:
    
      - `/warn` command (admin only) to give a warn to a member.
    
      - `/unwarn` command (admin only) to unwarn a member.
    
      - `/warns` command to show the number of warnings.
    
    Signed-off-by: Alviro Iskandar Setiawan <alviro.iskandar@gnuweeb.org>

--- index.ts
+++ index.ts
@@ -3,6 +3,7 @@ import { startCommand, versionCommand, pingCommand, helpCommand } from "./module
 import { banCommand, unbanCommand } from "./modules/admin/ban";
 import { kickCommand } from "./modules/admin/kick";
 import { muteCommand, unmuteCommand } from "./modules/admin/mute";
+import { warnCommand, unwarnCommand, warnsCommand } from "./modules/admin/warn";
 
 bot.command(startCommand.command, startCommand.function);
 bot.command(versionCommand.command, versionCommand.function);
@@ -15,6 +16,10 @@ bot.command(unbanCommand.command, unbanCommand.function);
 bot.command(kickCommand.command, kickCommand.function);
 bot.command(muteCommand.command, muteCommand.function);
 bot.command(unmuteCommand.command, unmuteCommand.function);
+
+bot.command(warnCommand.command, warnCommand.function);
+bot.command(unwarnCommand.command, unwarnCommand.function);
+bot.command(warnsCommand.command, warnsCommand.function);
 
 bot.launch();
 
